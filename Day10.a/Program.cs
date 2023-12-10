ce7891c01e31947a6241bb2a011199c2b43d574a
Tera <24725862+teraa@users.noreply.github.com>
2023-12-10 21:33:47 +0100

    day10

---
+++ Day10.a/Program.cs
@@ -0,0 +1,132 @@
+using System.Diagnostics;
+
+var values = GetInput()
+    .Select(line => line.Select(GetPipe)
+        .ToList())
+    .ToList();
+
+int size = values.Count;
+
+var points = new List<Point>();
+
+var start = values.Select((row, y) =>
+        row.Select((elem, x) => (y, x, elem))
+            .Where(e => e.elem is Direction.All)
+            .Select(e => new Point(e.x, e.y)))
+    .SelectMany(x => x)
+    .First();
+
+points.Add(start);
+
+var current = start;
+
+do
+{
+    Point? next = null;
+
+    var value = values[current.Y][current.X];
+
+    for (int y = -1; y <= 1; y++)
+    {
+        for (int x = -1; x <= 1; x++)
+        {
+            if (x == y || x == -y)
+                continue;
+
+            var offset = new Point(x, y);
+
+            var k = offset + current;
+
+            if (points.Count > 1 && points[^2] == k)
+                continue;
+
+            if (!IsInBounds(k))
+                continue;
+
+            var offsetDirection = OffsetToDirection(offset);
+            if (!value.HasFlag(offsetDirection))
+                continue;
+
+            var connectingDirection = GetConnectingDirection(offsetDirection);
+            var newValue = values[k.Y][k.X];
+
+            if (!newValue.HasFlag(connectingDirection))
+                continue;
+
+            next = k;
+            break;
+        }
+
+        if (next is not null)
+            break;
+    }
+
+    Debug.Assert(next is not null);
+
+    points.Add(next.Value);
+    current = next.Value;
+} while (current != start);
+
+WriteLine(points.Count / 2);
+return;
+
+static Direction GetConnectingDirection(Direction direction) => direction switch
+{
+    Direction.Up => Direction.Down,
+    Direction.Down => Direction.Up,
+    Direction.Right => Direction.Left,
+    Direction.Left => Direction.Right,
+    _ => throw new UnreachableException(),
+};
+
+static Direction OffsetToDirection(Point offset) => offset switch
+{
+    (-1, 0) => Direction.Left,
+    (1, 0) => Direction.Right,
+    (0, -1) => Direction.Up,
+    (0, 1) => Direction.Down,
+    _ => throw new UnreachableException(),
+};
+
+bool IsInBounds(Point point)
+    => point.X >= 0 && point.X < size && point.Y >= 0 && point.Y < size;
+
+static Direction GetPipe(char c) => c switch
+{
+    '|' => Direction.Vertical,
+    '-' => Direction.Horizontal,
+    'L' => Direction.UpRight,
+    'J' => Direction.UpLeft,
+    '7' => Direction.DownLeft,
+    'F' => Direction.DownRight,
+    '.' => Direction.None,
+    'S' => Direction.All,
+    _ => throw new UnreachableException(),
+};
+
+[Flags]
+enum Direction
+{
+    None = 0,
+    Up = 1 << 0,
+    Down = 1 << 1,
+    Left = 1 << 2,
+    Right = 1 << 3,
+    All = ~(-1 << 4),
+
+    Vertical = Up | Down,
+    Horizontal = Left | Right,
+    UpLeft = Up | Left,
+    UpRight = Up | Right,
+    DownLeft = Down | Left,
+    DownRight = Down | Right,
+}
+
+record struct Point(int X, int Y)
+{
+    public static Point operator +(Point a, Point b)
+        => new(a.X + b.X, a.Y + b.Y);
+
+    public static Point operator -(Point a, Point b)
+        => new(a.X - b.X, a.Y - b.Y);
+}
